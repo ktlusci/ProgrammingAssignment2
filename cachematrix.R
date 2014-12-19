3aee7f5970adccefd87b35f8ad33b6cf4374c05c
ktlusci <ktlusci@users.noreply.github.com>
2014-12-19 00:45:27 -0700

    Complete assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,36 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  xi <- NULL
+  
+  set <- function(xNew) {
+    x <<- xNew
+    xi <<- NULL    
+  }
+  
+  get <- function() x
+  setInv <- function(inverse) xi <<- inverse
+  getInv <- function() xi
+  
+  list(set = set, get = get, setInv = setInv, getInv = getInv)  
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  xi <- x$getInv()
+  if(!is.null(xi)) {
+    message("Retrieving cached inverse")
+    xi
+  }
+  else {
+    message("Calculating inverse")
+    x$setInv(solve(x$get()))
+    x$getInv()
+    
+  }
+  
 }
